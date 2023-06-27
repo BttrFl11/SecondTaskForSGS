9bd094d26b952fc05d85bc3bea766d07b0b3cd1f
BttrFl11 <bttrfl1585@gmail.com>
2023-06-27 16:08:39 +0300

    add space

--- Assets/Game/Scripts/Variant1/Rotateable.cs
+++ Assets/Game/Scripts/Variant1/Rotateable.cs
@@ -6,12 +6,13 @@ namespace Variant1
     {
         [SerializeField] private float _speed;
         [SerializeField] private Vector3 _direction;
+        [SerializeField] private Space _space;
 
         private Rotator _rotator;
 
         private void Awake()
         {
-            _rotator = new Rotator(transform, _direction, _speed);
+            _rotator = new Rotator(transform, _direction, _speed, _space);
         }
 
         private void Update()
--- Assets/Game/Scripts/Variant1/Rotator.cs
+++ Assets/Game/Scripts/Variant1/Rotator.cs
@@ -7,8 +7,9 @@ namespace Variant1
         private Transform _transform;
         private Vector3 _direction;
         private float _speed;
+        private Space _space;
 
-        public Rotator(Transform transform, Vector3 direction, float speed)
+        public Rotator(Transform transform, Vector3 direction, float speed, Space space)
         {
             _transform = transform;
             _direction = direction;
@@ -17,7 +18,7 @@ namespace Variant1
 
         public void Update()
         {
-            _transform.Rotate(_speed * Time.deltaTime * _direction, Space.Self);
+            _transform.Rotate(_speed * Time.deltaTime * _direction, _space);
         }
     }
 }
